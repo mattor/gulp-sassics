9b452f7be603572c678a38707892e6a613a487d6
Torbjørn Mathisen <herrmathisen@gmail.com>
2019-04-13 16:34:36 +0200

    Clean up

---
+++ index.js
@@ -0,0 +1,39 @@
+var through = require('through2');
+var path = require('path');
+var PluginError = require('plugin-error');
+
+// vars
+var PLUGIN_NAME = 'gulp-sassics';
+
+module.exports = function (search) {
+    search = search || 'ff0000';
+
+    return through.obj(function (file, encoding, callback) {
+        if (file.isNull()) {
+            callback(null, file);
+            return;
+        }
+
+        if (file.isStream()) {
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
+            return;
+        }
+
+        try {
+            var name = path.basename(file.path).slice(0, -4);
+            var outStr = String(file.contents);
+            outStr = encodeURIComponent(outStr);
+            outStr = outStr.replace(new RegExp(search, 'g'), '#{$color}');
+            outStr = '@function ' + name + '-icon($color: $base-icon-color) {\n\t@return "data:image/svg+xml,' + outStr + '";\n}\n';
+
+            file.contents = new Buffer(outStr);
+
+            callback(null, file);
+            return;
+        } catch (err) {
+            this.emit('error', new PluginError(PLUGIN_NAME, err, { fileName: file.path }));
+        }
+
+        callback(null, file);
+    });
+};
